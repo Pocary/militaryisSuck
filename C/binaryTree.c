0ada53201c3d4c037f791311531bb663a5b518ca
Pocary <105802972+Pocary@users.noreply.github.com>
2026-01-23 13:25:49 +0000

    [자료구조] 뭔가뭔가임 구현하기 귀찮다

--- C/binaryTree.c
+++ C/binaryTree.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
 typedef struct {
     int stu_num;
     int age;
-//   String name;
-//    bool sex;
+    char name[20];  // 동적할당하기 귀찮아서 이런거 아님
+    bool gender;    // 남 0 여 1
     int major;
     int year;
     int grade;
@@ -13,7 +15,7 @@ typedef struct {
 typedef struct {
     node* left;
     node* right;
-
+    stu_inf data;
 } node;
 
 int main() 
