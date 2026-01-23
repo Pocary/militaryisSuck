cc6e9a47efc5886fb063bdee8894e98f3c903c3f
Pocary <105802972+Pocary@users.noreply.github.com>
2026-01-23 08:21:10 +0000

    자료구조 뭔가를 해보려고 했음;

---
+++ C/binaryTree.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+typedef struct {
+    int stu_num;
+    int age;
+//   String name;
+//    bool sex;
+    int major;
+    int year;
+    int grade;
+} stu_inf;
+
+typedef struct {
+    node* left;
+    node* right;
+
+} node;
+
+int main() 
+{
+
+}
